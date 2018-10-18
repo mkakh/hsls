0ea59dcd29a84f60aaa9983e31cc8c682befb579
akh <akhs3214@gmail.com>
2018-10-18 11:02:16 +0900

    Refactor getCurrentDirContents

--- src/Lib.hs
+++ src/Lib.hs
@@ -5,7 +5,7 @@ import System.Directory
 import Data.List
 
 getCurrentDirContents :: IO String
-getCurrentDirContents = fmap toString $ fmap sort $ listDirectory =<< getCurrentDirectory where
+getCurrentDirContents = toString <$> sort <$> (listDirectory =<< getCurrentDirectory) where
   toString :: [String] -> String
   toString x = concat $ map addSpace x where
     addSpace :: String -> String
