50bb6d4b08078252891d0631eb2691ffc5ca9aee
akh <akhs3214@gmail.com>
2018-10-18 10:59:17 +0900

    sorted-ls

--- src/Lib.hs
+++ src/Lib.hs
@@ -2,9 +2,10 @@ module Lib
     ( getCurrentDirContents
     ) where
 import System.Directory
+import Data.List
 
 getCurrentDirContents :: IO String
-getCurrentDirContents = fmap toString $ listDirectory =<< getCurrentDirectory where
+getCurrentDirContents = fmap toString $ fmap sort $ listDirectory =<< getCurrentDirectory where
   toString :: [String] -> String
   toString x = concat $ map addSpace x where
     addSpace :: String -> String
