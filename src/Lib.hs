f1ebf22fffd2b4d5025896c6bcddbc80f8fc8b18
akh <akhs3214@gmail.com>
2018-10-18 10:31:23 +0900

    Refactor getCurrentDirContents

--- src/Lib.hs
+++ src/Lib.hs
@@ -4,11 +4,8 @@ module Lib
 import System.Directory
 
 getCurrentDirContents :: IO String
-getCurrentDirContents = fmap toString $ listDirectory =<< getCurrentDirectory
-
-toString :: [String] -> String
-toString x = concat $ map addSpace x where
-  addSpace :: String -> String
-  addSpace x = x ++ " "
-
-
+getCurrentDirContents = fmap toString $ listDirectory =<< getCurrentDirectory where
+  toString :: [String] -> String
+  toString x = concat $ map addSpace x where
+    addSpace :: String -> String
+    addSpace x = x ++ " "
