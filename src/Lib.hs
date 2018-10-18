9b3c7f8b994a710f5090d5b8c7808423464ac3f5
akh <akhs3214@gmail.com>
2018-10-18 10:29:40 +0900

    Remove unneeded texts

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,13 +1,9 @@
 module Lib
     ( getCurrentDirContents
     ) where
-
 import System.Directory
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
 
 getCurrentDirContents :: IO String
---ls = someFunc
 getCurrentDirContents = fmap toString $ listDirectory =<< getCurrentDirectory
 
 toString :: [String] -> String
